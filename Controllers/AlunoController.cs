a54f95b1bf60bb6d5c6b9ebcc14b9e381d0dc3e4
Henrique Leandro <miniqui10@gmail.com>
2020-09-05 18:42:24 -0230

    Atividade finalizada

---
+++ Controllers/AlunoController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Boletim.Domains;
+using API_Boletim.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_Boletim.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        // Chamara o repositório de Aluno
+        AlunoRepository repo = new AlunoRepository();
+
+
+        // GET: api/<AlunoController>
+        [HttpGet]
+        public List<Aluno> Get()
+        {
+            return repo.LerTodos();
+        }
+
+        // GET api/<AlunoController>/5
+        [HttpGet("{id}")]
+        public Aluno Get(int id)
+        {
+            return repo.BuscarPorId(id);
+        }
+
+        // POST api/<AlunoController>
+        [HttpPost]
+        public Aluno Post([FromBody] Aluno a)
+        {
+            return repo.Cadastrar(a);
+        }
+
+        // PUT api/<AlunoController>/5
+        [HttpPut("{id}")]
+        public Aluno Put(int id, [FromBody] Aluno a)
+        {
+            return repo.Alterar(id, a);
+        }
+
+        // DELETE api/<AlunoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            repo.Excluir(id);
+
+        }
+    }
+}
---
+++ Repositories/AlunoRepository.cs
@@ -0,0 +1,129 @@
+﻿using API_Boletim.Context;
+using API_Boletim.Domains;
+using API_Boletim.Interfaces;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Boletim.Repositories
+{
+    public class AlunoRepository : IAluno
+    {
+        // Chamar a classe de conexão do banco
+        BoletimContext conexao = new BoletimContext();
+
+        // Chamar o objeto responsável por receber e executar as linhas de comandos do banco
+        SqlCommand cmd = new SqlCommand();
+
+
+        public Aluno Alterar(int id, Aluno a)
+        {
+            cmd.Connection = conexao.Conectar();
+
+            cmd.CommandText = "UPDATE Aluno SET " +
+                "Nome = @nome, " +
+                "Ra = @ra, " +
+                "Idade = @idade WHERE IdAluno = @id ";
+
+            cmd.Parameters.AddWithValue("@nome", a.Nome);
+            cmd.Parameters.AddWithValue("@ra", a.RA);
+            cmd.Parameters.AddWithValue("@idade", a.Idade);
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            cmd.ExecuteNonQuery();
+
+            conexao.Desconectar();
+            return a;
+        }
+
+        public Aluno BuscarPorId(int id)
+        {
+            cmd.Connection = conexao.Conectar();
+            cmd.CommandText = "SELECT * FROM Aluno WHERE IdAluno = @id";
+            cmd.Parameters.AddWithValue("@id", id);
+
+            SqlDataReader dados = cmd.ExecuteReader();
+
+            Aluno a = new Aluno();
+
+                while(dados.Read())
+            {
+                a.IdAluno = Convert.ToInt32(dados.GetValue(0));
+                a.Nome = dados.GetValue(1).ToString();
+                a.RA = dados.GetValue(2).ToString();
+                a.Idade = Convert.ToInt32(dados.GetValue(3));
+
+            }
+
+            conexao.Desconectar();
+
+            return a;
+        }
+
+        public Aluno Cadastrar(Aluno a)
+        {
+            cmd.Connection = conexao.Conectar();
+
+            cmd.CommandText =
+                "INSERT INTO Aluno (Nome, RA, Idade) " +
+                "VALUES" +
+                "(@nome, @ra, @idade)";
+            cmd.Parameters.AddWithValue("@nome", a.Nome);
+            cmd.Parameters.AddWithValue("@ra", a.RA);
+            cmd.Parameters.AddWithValue("@idade", a.Idade);
+
+            // Será este comando o responsável por injetar os dados no banco efetivamente
+            cmd.ExecuteNonQuery();
+
+            conexao.Desconectar();
+            return a;
+        }
+
+        public void Excluir(int id)
+        {
+            cmd.Connection = conexao.Conectar();
+
+            cmd.CommandText = "DELETE FROM Aluno WHERE IdAluno = @id";
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+
+            conexao.Desconectar();
+        }
+
+        public List<Aluno> LerTodos()
+        {
+            // Abrir conexao
+            cmd.Connection = conexao.Conectar();
+
+            // Preparar a query(Consulta)
+            cmd.CommandText = "SELECT * FROM Aluno";
+
+            SqlDataReader dados = cmd.ExecuteReader();
+
+            // Criar lista para guardar Alunos
+            List<Aluno> alunos = new List<Aluno>();
+
+            while(dados.Read())
+            {
+                alunos.Add(
+                    new Aluno()
+                    {
+                        IdAluno     = Convert.ToInt32(dados.GetValue(0)),
+                        Nome        = dados.GetValue(1).ToString(),
+                        RA          = dados.GetValue(2).ToString(),
+                        Idade       = Convert.ToInt32(dados.GetValue(3))
+                    }
+                    );
+            }
+
+            // fechar conexao
+            conexao.Desconectar();
+
+            return alunos;
+        }
+    }
+}
